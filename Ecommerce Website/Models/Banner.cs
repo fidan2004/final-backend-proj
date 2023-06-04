d505a7f13310133841d04ef5435afffd2ae3fbd3
fidan2004 <fidankk@code.edu.az>
2023-06-05 01:45:33 +0400

    backendd

---
+++ Ecommerce Website/Models/Banner.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce_Website.Models
+{
+    public class Banner:BaseEntity<int>
+    {   
+        public string Label { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Image { get; set; } = string.Empty;
+    }
+}
---
+++ Ecommerce Website/Models/HomeFirstSlider.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using Ecommerce_Website.Models;
+
+public class HomeFirstSlider:BaseEntity<int>
+{
+    public string Precent { get; set; } = string.Empty;
+    public string Label { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Image { get; set; } = string.Empty;
+}
